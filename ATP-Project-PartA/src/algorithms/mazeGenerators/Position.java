e3bd894943ce35dafadc5703ef5ffd49e4595197
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-17 15:30:17 +0300

    Slowly updating Position

--- ATP-Project-PartA/src/algorithms/mazeGenerators/Position.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/Position.java
@@ -1,4 +1,20 @@
 package algorithms.mazeGenerators;
 
+/** A class representing the position */
 public class Position {
+    private int row, column;
+
+    /** Position constructor using the current Row and Column */
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getStartPosition(){
+        return 0;
+    }
+
+    public int getGoalPosition(){
+        return 0;
+    }
 }
