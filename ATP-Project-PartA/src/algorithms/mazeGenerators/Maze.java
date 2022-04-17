92983ce60cd5ae2cc86ef7e85b8dae9f214a4d75
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-17 15:02:49 +0300

    Added functions to maze and added position class

--- ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
@@ -2,12 +2,14 @@ package algorithms.mazeGenerators;
 
 public class Maze {
 
+    /** Every maze is built out of a 2-dimensional array of ints. 1 means it's a wall, 0 means it's a path.*/
     int[][] maze;
 
     public Maze(int[][] mz) {
         this.maze = mz;
     }
 
+    /** Function that gets us the protected array of the class*/
     public int[][] getMaze() {
         return maze;
     }
@@ -18,5 +20,16 @@ public class Maze {
 
     public void setVal(int row, int column, int val){
         this.getMaze()[row][column]=val;
+    }
+
+    public int getRowNum(){
+        return getMaze().length;
+    }
+    public int getColNum(){
+        return getMaze()[0].length;
+    }
+    public void print(){
+        for(int i=0;i<this.get)
+        System.out.println(this.getMaze());
     }
 }
