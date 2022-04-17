37752a5be854f8f2b3b3c00fc2314b87e5bd18f2
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-17 15:17:42 +0300

    Added documentation to Maze and fixed the "print" function

--- ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
@@ -1,35 +1,42 @@
 package algorithms.mazeGenerators;
 
+import java.util.Arrays;
+
+/** Every maze is built out of a 2-dimensional array of ints. 1 means it's a wall, 0 means it's a path.*/
 public class Maze {
-
-    /** Every maze is built out of a 2-dimensional array of ints. 1 means it's a wall, 0 means it's a path.*/
+    //TODO privatize maze
     int[][] maze;
-
+    /** Constructor of the maze*/
     public Maze(int[][] mz) {
         this.maze = mz;
     }
 
-    /** Function that gets us the protected array of the class*/
+    /** Function that gets us the private array of the class*/
     public int[][] getMaze() {
         return maze;
     }
 
+    /** Given the row and column numbers, return the current value within that spot.*/
     public int getVal(int row, int column) {
         return this.getMaze()[row][column];
     }
 
+    /** Given the row and column numbers, sets the given value into that spot in the maze.*/
     public void setVal(int row, int column, int val){
         this.getMaze()[row][column]=val;
     }
 
+    /** Gets the number of rows*/
     public int getRowNum(){
         return getMaze().length;
     }
+    /** Gets the number of columns*/
     public int getColNum(){
         return getMaze()[0].length;
     }
+    /** Prints the array to the system*/
     public void print(){
-        for(int i=0;i<this.get)
-        System.out.println(this.getMaze());
+        for(int i=0;i<this.getRowNum()-1;i++)
+            System.out.println(Arrays.toString(this.getMaze()[i]));
     }
 }
