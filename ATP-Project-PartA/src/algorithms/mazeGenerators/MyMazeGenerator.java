fb3173930bdd2ff4ada6ade760fa3bef9f6c5d60
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-17 14:49:11 +0300

    Github

--- ATP-Project-PartA/src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -7,11 +7,15 @@ public class MyMazeGenerator extends AMazeGenerator {
     @Override
     public Maze generate(int row, int column) {
         // Begin with the maze's space with no walls. Call this a chamber.
-        Maze maze = null;
+        Maze maze = new EmptyMazeGenerator().generate(row, column);
         // Divide the chamber with a randomly positioned wall (or multiple walls) where each wall
         // contains a randomly positioned passage opening within it.
         int row_d = (int) (Math.random() * row + 1);
         int column_d = (int) (Math.random() * column + 1);
+        for (int i = 0; i < column; i++)
+            maze.setVal(i, column, 1);
+        for (int i = 0; i < row; i++)
+            maze.setVal(row, i, 1);
         // Then recursively repeat the process on the sub-chambers until all chambers are minimum sized.
 
         return maze;
