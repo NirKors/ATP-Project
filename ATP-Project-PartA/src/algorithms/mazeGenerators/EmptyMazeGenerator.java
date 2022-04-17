0c97c27288100f679e38ff7659ccc5f176c9901b
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-17 16:51:31 +0300

    EmptyMazeGenerator start / goal generation, Updates to MyMazeGenerator.divide

--- ATP-Project-PartA/src/algorithms/mazeGenerators/EmptyMazeGenerator.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/EmptyMazeGenerator.java
@@ -8,17 +8,30 @@ public class EmptyMazeGenerator extends AMazeGenerator {
             for (int j = 0; j < column; j++)
                 maze.setVal(i, j, 0);
         }
-        int srow = 0, scolumn = 0, erow = 0, ecolumn = 0;
+        int start_row = 0, goal_row = 0, start_column = 0, goal_column = 0;
 
-        while (erow == srow && scolumn == ecolumn) {
-            srow = (int) (Math.random() * row + 1);
-            scolumn = (int) (Math.random() * column + 1);
-            srow = (int) (Math.random() * row + 1);
-            scolumn = (int) (Math.random() * column + 1);
+
+        // Random start / goal generation:
+        while (start_row == goal_row && start_column == goal_column) {
+            if (Math.random() < 0.5) {
+                start_row = (int) (Math.random() * row + 1);
+                start_column = Math.random() < 0.5 ? 0 : column;
+            } else {
+                start_column = (int) (Math.random() * column + 1);
+                start_row = Math.random() < 0.5 ? 0 : row;
+            }
+
+            if (Math.random() < 0.5) {
+                goal_row = (int) (Math.random() * row + 1);
+                goal_column = Math.random() < 0.5 ? 0 : column;
+            } else {
+                goal_column = (int) (Math.random() * column + 1);
+                goal_row = Math.random() < 0.5 ? 0 : row;
+            }
         }
 
-        maze.setStartPosition(new Position(srow, scolumn));
-        maze.setGoalPosition(new Position(erow, ecolumn));
+        maze.setStartPosition(new Position(start_row, start_column));
+        maze.setGoalPosition(new Position(goal_row, goal_column));
 
         return maze;
     }
--- ATP-Project-PartA/src/algorithms/mazeGenerators/MyMazeGenerator.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/MyMazeGenerator.java
@@ -13,17 +13,27 @@ public class MyMazeGenerator extends AMazeGenerator {
         return maze;
     }
 
-    //TODO: Documentation and wall passage.
+    /**
+     * Recursive division method used to build walls and necessary holes in those walls.
+     *
+     * @param maze         The maze we'll recursively divide.
+     * @param row_start    Starting row position.
+     * @param row_size     Row size.
+     * @param column_start Starting Column Position.
+     * @param column_size  Column size.
+     */
     private void divide(Maze maze, int row_start, int row_size, int column_start, int column_size) {
         if (row_size <= 1 || column_size <= 1) return; // recursion condition
 
-        int wall;
+        int wall, hole;
         // Adds a vertical wall.
         if (column_size > row_size) {
             wall = (int) (Math.random() * column_size + column_start);
+            hole = (int) (Math.random() * row_size + row_start);
             for (int row = row_start; row < column_size; row++) {
                 maze.setVal(row, wall, 1);
             }
+            maze.setVal(hole, wall, 0);
             divide(maze, row_start, row_size - wall, column_start, column_size);
             divide(maze, wall, row_size - wall, column_start, column_size);
 
@@ -31,9 +41,11 @@ public class MyMazeGenerator extends AMazeGenerator {
         // Adds a horizontal wall.
         else {
             wall = (int) (Math.random() * row_size + row_start);
+            hole = (int) (Math.random() * column_size + column_start);
             for (int column = column_start; column < row_size; column++) {
                 maze.setVal(wall, column, 1);
             }
+            maze.setVal(wall, hole, 0);
             divide(maze, row_start, row_size, column_start, column_size - wall);
             divide(maze, row_start, row_size, wall, column_size - wall);
         }
