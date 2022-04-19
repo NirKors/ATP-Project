1b5641a7f3054c4cfd0a71c9fa02d161e5e0b2e8
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-19 11:43:14 +0300

    SimpleMazeGenerator now works as intended, making sure there is at least one path. Added comfortable position changers kn Position.

--- ATP-Project-PartA/src/algorithms/search/AState.java
+++ ATP-Project-PartA/src/algorithms/search/AState.java
@@ -1,4 +1,9 @@
 package algorithms.search;
 
+import algorithms.mazeGenerators.Position;
+
+import java.util.ArrayList;
+
 public abstract class AState {
+    public ArrayList<Position> possible_directions;
 }
