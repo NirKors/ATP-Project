71b7fbb8ef43929a1ef1f18e927b8fe3fa8e14ac
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-19 14:08:40 +0300

    Built skeleton of mission B

--- ATP-Project-PartA/src/algorithms/search/DepthFirstSearch.java
+++ ATP-Project-PartA/src/algorithms/search/DepthFirstSearch.java
@@ -1,4 +1,18 @@
 package algorithms.search;
 
-public class DepthFirstSearch {
+public class DepthFirstSearch implements ISearchingAlgorithm {
+    @Override
+    public Solution solve(ISearchable domain) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int getNumberOfNodesEvaluated() {
+        return 0;
+    }
 }
