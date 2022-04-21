4ef81cf585d783e5bc23c4cab1863e4c15e46569
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-21 12:24:32 +0300

    BreadthFirstSearch + Solution.setSolution + Documentations

--- ATP-Project-PartA/src/algorithms/search/BreadthFirstSearch.java
+++ ATP-Project-PartA/src/algorithms/search/BreadthFirstSearch.java
@@ -2,6 +2,7 @@ package algorithms.search;
 
 import java.util.ArrayList;
 
+
 public class BreadthFirstSearch extends ASearchingAlgorithm {
     @Override
     public Solution solve(ISearchable domain) {
@@ -13,28 +14,54 @@ public class BreadthFirstSearch extends ASearchingAlgorithm {
         Q.add(current_state);
         visited.add(current_state);
         while (!Q.isEmpty()) {
-            current_state = Q.remove(0); // FIFO?
+            current_state = Q.remove(0);
             v.addState(current_state);
 
-            if (current_state.equals(domain.getGoal()))
+            if (current_state.equals(domain.getGoal())) { // Reached goal.
+                v.setSolution(cleanPath(v));
                 return v;
+            }
 
             boolean flag = false;
             for (AState state : domain.getAllPossibleStates(current_state)) {
-                if (flag) {
+                if (flag) { // Skips diagonal paths.
                     flag = false;
                     continue;
                 }
-                if ((!visited.contains(state)) && domain.isIn(state)) {
+
+                if ((!visited.contains(state)) && domain.isIn(state)) { // Checks if a valid, unvisited node.
                     nodes_Evaluated++;
                     visited.add(state);
                     Q.add(state);
                 }
                 flag = true;
             }
+
         }
         return v;
     }
+
+    /**
+     * Makes sure Solution v contains a valid path, by removing unnecessary node traversals.
+     *
+     * @param v - Solution to filter
+     * @return Filtered solution
+     */
+    private ArrayList<AState> cleanPath(Solution v) {
+        ArrayList<AState> path = v.getSolutionPath();
+        int counter = path.size();
+        while (counter > 2) {
+            AState curr = path.get(--counter);
+            AState prev = path.get(counter - 1);
+            int diffRow = Math.abs(curr.pos.getRowIndex() - prev.pos.getRowIndex());
+            int diffCol = Math.abs(curr.pos.getColumnIndex() - prev.pos.getColumnIndex());
+            if (diffRow > 1 || diffCol > 1 || diffRow + diffCol == 2)
+                path.remove(prev);
+
+        }
+        return path;
+    }
+
 
     @Override
     public String getName() {
