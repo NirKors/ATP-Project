b8ed6064384bd5b9025783b57f3ce3b50f708ef0
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-22 17:28:20 +0300

    AMaze3DGenerator.generateStartGoal

--- ATP-Project-PartA/src/algorithms/maze3D/AMaze3DGenerator.java
+++ ATP-Project-PartA/src/algorithms/maze3D/AMaze3DGenerator.java
@@ -12,37 +12,14 @@ public abstract class AMaze3DGenerator implements IMaze3DGenerator {
     }
 
     protected void generateStartGoal(Maze3D maze) {
+        int depth = maze.getDepthNum(), row = maze.getRowNum(), col = maze.getColNum();
+        Position3D start = new Position3D((int)(Math.random() * depth), (int)(Math.random() * row), (int)(Math.random() * col));
+        Position3D goal = new Position3D((int)(Math.random() * depth), (int)(Math.random() * row), (int)(Math.random() * col));
 
-        // Random start / goal generation:
-        int start_depth = 0, goal_depth = 0, start_row = 0, goal_row = 0, start_column = 0, goal_column = 0;
-        int targetStart = 0, targetGoal = 0;
-        int depth = maze.getDepthNum(), row = maze.getRowNum(), column = maze.getColNum();
-        while ((start_depth == goal_depth && start_row == goal_row && start_column == goal_column) || (targetStart != 0 || targetGoal != 0)) {
-
-            if (Math.random() < 0.5) {
-                start_row = (int) (Math.random() * row);
-                start_column = Math.random() < 0.5 ? 0 : column - 1;
-            } else {
-                start_column = (int) (Math.random() * column);
-                start_row = Math.random() < 0.5 ? 0 : row - 1;
-            }
-            start_depth = (int) (Math.random() * depth);
-
-            if (Math.random() < 0.5) {
-                goal_row = (int) (Math.random() * row);
-                goal_column = Math.random() < 0.5 ? 0 : column - 1;
-            } else {
-                goal_column = (int) (Math.random() * column);
-                goal_row = Math.random() < 0.5 ? 0 : row - 1;
-            }
-            goal_depth = (int) (Math.random() * depth);
-            targetStart = maze.getVal(start_depth, start_row, start_column);
-            targetGoal = maze.getVal(start_depth, goal_row, goal_column);
-        }
-        maze.setVal(start_depth, start_row, start_column, 0);
-        maze.setVal(goal_depth, goal_row, goal_column, 0);
-        maze.setStartPosition(new Position3D(start_depth, start_row, start_column));
-        maze.setGoalPosition(new Position3D(goal_depth, goal_row, goal_column));
+        maze.setVal(start, 0);
+        maze.setVal(goal, 0);
+        maze.setStartPosition(start);
+        maze.setGoalPosition(goal);
     }
 
 }
