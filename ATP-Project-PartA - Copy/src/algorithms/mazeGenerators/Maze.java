c8e4e08c5e26d5e505d19ff6d5d35ab0bea83be8
NirKors <103315566+NirKors@users.noreply.github.com>
2022-04-22 16:12:27 +0300

    File fix + BestFirstSearch fix

--- ATP-Project-PartA - Copy/src/algorithms/mazeGenerators/Maze.java
+++ ATP-Project-PartA - Copy/src/algorithms/mazeGenerators/Maze.java
@@ -1,9 +1,104 @@
 package algorithms.mazeGenerators;
 
+/**
+ * Every maze is built out of a 2-dimensional array of ints. 1 means it's a wall, 0 means it's a path.
+ */
 public class Maze {
 
-    int [][] maze;
-    public Maze(int[][] mz){
-        this.maze = mz;
+    private int[][] maze;
+    private Position startPos, goalPos;
+
+    /**
+     * Constructor of the maze
+     */
+    public Maze(int row_size, int column_size) {
+        maze = new int[row_size][column_size];
+    }
+
+
+    /**
+     * Given the row and column numbers, return the current value within that spot. Returns -1 if value is off limits.
+     */
+    public int getVal(int row, int column) {
+        if (row < 0 || column < 0 || row >= this.getRowNum() || column >= this.getColNum())
+            return -1;
+        return this.maze[row][column];
+    }
+
+    /**
+     * Given the row and column numbers or a position, sets the given value into that spot in the maze.
+     */
+    public void setVal(int row, int column, int val) {
+        maze[row][column] = val;
+    }
+
+    public void setVal(Position pos, int val) {
+        maze[pos.getRowIndex()][pos.getColumnIndex()] = val;
+    }
+
+    /**
+     * Gets the number of rows
+     */
+    public int getRowNum() {
+        return maze.length;
+    }
+
+    /**
+     * Gets the number of columns
+     */
+    public int getColNum() {
+        return maze[0].length;
+    }
+
+    /**
+     * Prints the array to the system
+     */
+    public void print() {
+        //Unable to use the "substring" method without initializing str
+        String str = null;
+        for (int row = 0; row < this.getRowNum(); row++) {
+            str = "{";
+            for (int col = 0; col < this.getColNum(); col++) {
+                if (row == startPos.getRowIndex() && col == startPos.getColumnIndex()) {
+                    str += "S,";
+                } else if (row == this.getGoalPosition().getRowIndex() && col == this.getGoalPosition().getColumnIndex()) {
+                    str += "E,";
+                } else {
+                    str += getVal(row, col) + ",";
+                }
+            }
+            str = str.substring(0, str.length() - 1);
+            str += "}";
+            System.out.println(str);
+        }
+
+    }
+
+    /**
+     * Gets the starting position
+     */
+    public Position getStartPosition() {
+        return startPos;
+    }
+
+    /**
+     * Gets the goal position
+     */
+    public Position getGoalPosition() {
+        return goalPos;
+    }
+
+    /**
+     * Sets the starting position
+     */
+    public void setStartPosition(Position p) {
+        startPos = p;
+    }
+
+    /**
+     * Sets the goal position
+     */
+    public void setGoalPosition(Position p) {
+        goalPos = p;
     }
 }
--- ATP-Project-PartA - Copy/src/algorithms/mazeGenerators/SimpleMazeGenerator.java
+++ ATP-Project-PartA - Copy/src/algorithms/mazeGenerators/SimpleMazeGenerator.java
@@ -2,8 +2,46 @@ package algorithms.mazeGenerators;
 
 public class SimpleMazeGenerator extends AMazeGenerator {
 
+    /**
+     * Randomely puts numbers in different places
+     */
     @Override
     public Maze generate(int row, int column) {
-        return null; //TODO
+        // Start by creating a random
+        Maze maze = new Maze(row, column);
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                maze.setVal(i, j, (int) Math.round(Math.random()));
+            }
+        }
+        generateStartGoal(maze);
+        // We generated a maze with a start and end position, with random walls/paths.
+        // The below function will make sure that there is at least a single path between these positions.
+        int curr_row = maze.getStartPosition().getRowIndex(), curr_col = maze.getStartPosition().getColumnIndex(),
+                goal_row = maze.getGoalPosition().getRowIndex(), goal_column = maze.getGoalPosition().getColumnIndex();
+        while (true) {
+            if (curr_row == goal_row && curr_col == goal_column) {
+                break;
+            }
+            // Using math.random, we will decide if to get closer by column or row
+            if (Math.random() < 0.5) {
+                // Checking by columns
+                if (curr_col < goal_column) {
+                    maze.setVal(curr_row, ++curr_col, 0);
+                }
+                if (curr_col > goal_column) {
+                    maze.setVal(curr_row, --curr_col, 0);
+                }
+            } else {
+                if (curr_row < goal_row) {
+                    maze.setVal(++curr_row, curr_col, 0);
+                }
+                if (curr_row > goal_row) {
+                    maze.setVal(--curr_row, curr_col, 0);
+                }
+            }
+        }
+
+        return maze;
     }
 }
