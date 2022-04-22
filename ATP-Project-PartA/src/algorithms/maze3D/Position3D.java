90178e8b43bd402bb1547d9905a8838db31eaa89
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-22 16:59:31 +0300

    Position3D additions

--- ATP-Project-PartA/src/algorithms/maze3D/Position3D.java
+++ ATP-Project-PartA/src/algorithms/maze3D/Position3D.java
@@ -1,5 +1,7 @@
 package algorithms.maze3D;
 
+import algorithms.mazeGenerators.Position;
+
 public class Position3D {
     private int depth, row, column;
 
@@ -38,5 +40,41 @@ public class Position3D {
      */
     public String toString() {
         return "{" + this.getDepthIndex() + "," + this.getRowIndex() + "," + this.getColumnIndex() + "}";
+    }
+
+    // To ease out movements and states, we will add functions that give a new position depending on where we want to go
+    public Position3D Up() {
+        return new Position3D(this.getDepthIndex(), this.getRowIndex() + 1, this.getColumnIndex());
+    }
+
+    public Position3D Down() {
+        return new Position3D(this.getDepthIndex(), this.getRowIndex() - 1, this.getColumnIndex());
+    }
+
+    public Position3D Left() {
+        return new Position3D(this.getDepthIndex(), this.getRowIndex(), this.getColumnIndex() - 1);
+    }
+
+    public Position3D Right() {
+        return new Position3D(this.getDepthIndex(), this.getRowIndex(), this.getColumnIndex() + 1);
+    }
+
+    public Position3D Above() {
+        return new Position3D(this.getDepthIndex() + 1, this.getRowIndex(), this.getColumnIndex());
+    }
+
+    public Position3D Below() {
+        return new Position3D(this.getDepthIndex() - 1, this.getRowIndex(), this.getColumnIndex());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Position3D))
+            return false;
+
+        Position3D p = (Position3D) o;
+        return p.getRowIndex() == row && p.getColumnIndex() == column && p.getDepthIndex() == depth;
     }
 }
