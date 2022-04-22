7ade30d253096f6cff578d0796f5afe0f62c44a5
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-22 16:13:30 +0300

    Out

--- ATP-Project-PartA/src/algorithms/search/DepthFirstSearch.java
+++ ATP-Project-PartA/src/algorithms/search/DepthFirstSearch.java
@@ -14,10 +14,11 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
         // prevDict is a dictionary where the keys are the states, and the values are the states they came from in the path /
         Hashtable<AState, AState> prevDict = new Hashtable<>();
         S.add(domain.getStart());
+        nodes_Evaluated--;// we don't want the first position to count towards nodes evaluated
         //Here we do the dfs search itself
         while (!S.isEmpty()) {
             current_state = S.get(S.size() - 1);
-            if (current_state == domain.getGoal()) {
+            if (current_state.equals(domain.getGoal())) {
                 break;
             }
             S.remove(S.size() - 1);
@@ -29,12 +30,12 @@ public class DepthFirstSearch extends ASearchingAlgorithm {
                 if (!(visited.contains(state)) && domain.isIn(state)) {
                     visited.add(state);
                     S.add(state);
-
                     prevDict.put(state, current_state);
-                    nodes_Evaluated++;
                 }
             }
+            nodes_Evaluated++;
         }
+        nodes_Evaluated--;
         ArrayList<AState> reverser = new ArrayList<>();
         reverser.add(domain.getGoal());
         current_state = domain.getGoal();
