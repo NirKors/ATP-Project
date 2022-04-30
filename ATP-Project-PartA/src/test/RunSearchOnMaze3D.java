b68c1fca0ccfda77524897f52e61b8f101c7fb11
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-04-30 13:14:07 +0300

    Added RunSearchOnMaze3D and changed RunSearchOnMaze & SearchableMaze3D, so they will work

--- ATP-Project-PartA/src/test/RunSearchOnMaze3D.java
+++ ATP-Project-PartA/src/test/RunSearchOnMaze3D.java
@@ -1,5 +1,9 @@
 package test;
 
+import algorithms.maze3D.IMaze3DGenerator;
+import algorithms.maze3D.Maze3D;
+import algorithms.maze3D.MyMaze3DGenerator;
+import algorithms.maze3D.SearchableMaze3D;
 import algorithms.mazeGenerators.IMazeGenerator;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.MyMazeGenerator;
@@ -7,12 +11,12 @@ import algorithms.search.*;
 
 import java.util.ArrayList;
 
-public class RunSearchOnMaze {
+public class RunSearchOnMaze3D {
     public static void main(String[] args) {
-        IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(5, 5);
+        IMaze3DGenerator mg = new MyMaze3DGenerator();
+        Maze3D maze = mg.generate(5, 5,5);
         maze.print();
-        SearchableMaze searchableMaze = new SearchableMaze(maze);
+        SearchableMaze3D searchableMaze = new SearchableMaze3D(maze);
         solveProblem(searchableMaze, new BreadthFirstSearch());
         solveProblem(searchableMaze, new DepthFirstSearch());
         solveProblem(searchableMaze, new BestFirstSearch());
