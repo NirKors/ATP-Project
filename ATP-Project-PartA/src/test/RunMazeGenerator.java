751f3769d719b3573c1211755ea36cac2597c153
NirKors <103315566+NirKors@users.noreply.github.com>
2022-05-16 15:39:18 +0300

    Doc folder

--- ATP-Project-PartA/src/test/RunMazeGenerator.java
+++ ATP-Project-PartA/src/test/RunMazeGenerator.java
@@ -8,9 +8,9 @@ public class RunMazeGenerator {
     }
     private static void testMazeGenerator(IMazeGenerator mazeGenerator) {
 // prints the time it takes the algorithm to run
-        System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(4/*rows*/, 4/*columns*/)));
+        System.out.println(String.format("Maze generation time(ms): %s", mazeGenerator.measureAlgorithmTimeMillis(100/*rows*/, 100/*columns*/)));
 // generate another maze
-        Maze maze = mazeGenerator.generate(4/*rows*/, 4/*columns*/);
+        Maze maze = mazeGenerator.generate(100/*rows*/, 100/*columns*/);
 // prints the maze
         maze.print();
 // get the maze entrance
