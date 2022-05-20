76f2a81d9a44d1a2d3bbca2b5be595986e42dbc4
NirKors <103315566+NirKors@users.noreply.github.com>
2022-05-20 19:11:08 +0300

    Maze.Maze & Maze.toByteArray & (de)Compressor

---
+++ ATP-Project-PartA/src/IO/MyCompressorOutputStream.java
@@ -0,0 +1,39 @@
+package IO;
+
+import java.io.*;
+
+public class MyCompressorOutputStream extends OutputStream {
+    OutputStream out;
+
+    public MyCompressorOutputStream(OutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void write(int b) {
+    }
+
+
+    @Override
+    public void write(byte[] b) { //TODO: 255 limit
+        if (b.length == 0)
+            return;
+
+        byte singular = 0;
+        int count = 0;
+
+        for (int i = 1; i < b.length; i++) {
+            if (singular == b[i] && i != b.length - 1)
+                count++;
+            else {
+                write(count);
+
+                if (singular == 0)
+                    singular = 1;
+                else
+                    singular = 0;
+                count = 1;
+            }
+        }
+    }
+}
--- ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
+++ ATP-Project-PartA/src/algorithms/mazeGenerators/Maze.java
@@ -1,5 +1,7 @@
 package algorithms.mazeGenerators;
 
+import java.util.ArrayList;
+
 /**
  * Every maze is built out of a 2-dimensional array of ints. 1 means it's a wall, 0 means it's a path.
  */
@@ -14,6 +16,44 @@ public class Maze {
     public Maze(int row_size, int column_size) {
         maze = new int[row_size][column_size];
     }
+
+    public Maze(byte[] b) { // TODO: Test if works.
+        int row = 0, col = 0;
+        for (byte bb : b) {
+            if (bb == -1) {
+                row++;
+            } else if (bb < 0)
+                col++;
+            else
+                col += bb;
+        }
+        maze = new int[row][col];
+        row = 0;
+        for (byte cell : b) {
+            col = 0;
+            while (cell != -1) {
+                switch (cell) {
+                    case 0:
+                    case 1:
+                        for (int j = 0; j < cell; j++)
+                            maze[row][col++] = cell;
+                        break;
+                    case -2:
+                        startPos = new Position(row, col);
+                        maze[row][col++] = 0;
+                        break;
+                    case -3:
+                        goalPos = new Position(row, col);
+                        maze[row][col++] = 0;
+                        break;
+                    case -4:
+                        break;
+                }
+            }
+            row++;
+        }
+    }
+
 
     /**
      * Given the row and column numbers, return the current value within that spot. Returns -1 if value is off limits.
@@ -103,5 +143,68 @@ public class Maze {
      */
     public void setGoalPosition(Position p) {
         goalPos = p;
+    }
+    /**
+     * Maze will be compressed using negative bytes as flags.
+     * <p>
+     *     Alternating between 0 and 1, starting with 0 we return the length of consecutive byte in the array.
+     *     While 0 is empty space, 1 is wall, -1 is a new row, -2 is the starting position, -3 is the end position, and -4 signifies continuing the previous byte (used in cases where sequence is larger than 255).
+     *     <p>After an encounter with new row (-1) alternation restarts with 0.</p>
+     * </p>
+     *
+     *<pre>
+     * Example:
+     *     For the maze [[1, E], [0, S]]
+     *     return array = [0, 1, -3, -1, 1, -2, -1].
+     *</pre>
+     * @return Compressed byte array.
+     */
+    public byte[] toByteArray() {
+        ArrayList<Byte> toReturn = new ArrayList<>();
+        byte count;
+        int prev = 0;
+        int srow = startPos.getRowIndex(), scol = startPos.getColumnIndex();
+        int grow = goalPos.getRowIndex(), gcol = goalPos.getColumnIndex();
+
+        for (int row = 0; row < maze.length; row++) {
+            count = 0;
+            for (int col = 0; col < maze[0].length; col++) {
+                if (row == srow && col == scol) {
+                    toReturn.add(count);
+                    toReturn.add((byte) -2);
+                    count = 0;
+                } else if (row == grow && col == gcol) {
+                    toReturn.add(count);
+                    toReturn.add((byte) -3);
+                    count = 0;
+                } else{
+                    if (maze[row][col] == prev && count != 255)
+                        count++;
+                    else{
+                        toReturn.add(count);
+                        count = 0;
+                        if (count == 255){
+                            toReturn.add((byte) -4);
+                        }
+                        else{
+                            count++;
+                            if (prev == 0)
+                                prev = 1;
+                            else
+                                prev = 0;
+                        }
+                    }
+                }
+            }
+            toReturn.add((byte) -1);
+            prev = 0;
+        }
+
+        // Conversion to array:
+        byte[] array = new byte[toReturn.size()];
+        for (int i = 0; i < toReturn.size(); i++) {
+            array[i] = toReturn.get(i);
+        }
+        return array;
     }
 }
