769c2ae0808e66662c1a4fbba3ef139929cba44a
NirKors <103315566+NirKors@users.noreply.github.com>
2022-05-21 18:11:40 +0300

    My(de)Compressor

--- ATP-Project-PartA/src/IO/MyCompressorOutputStream.java
+++ ATP-Project-PartA/src/IO/MyCompressorOutputStream.java
@@ -9,25 +9,59 @@ public class MyCompressorOutputStream extends OutputStream {
         this.out = out;
     }
 
-    @Override
-    public void write(int b) { //TODO check if even needed
+    /**
+     * Helper function
+     */
+    private void write(byte b, int amount) throws IOException {
+        do {
+            out.write(b);
+            out.write(Math.min(255, amount));
+            amount -= 255;
+        } while (amount > 255);
     }
 
-
+    /**
+     * Writes the specified byte to this output stream. The general
+     * contract for {@code write} is that one byte is written
+     * to the output stream. The byte to be written is the eight
+     * low-order bits of the argument {@code b}. The 24
+     * high-order bits of {@code b} are ignored.
+     * <p>
+     * Subclasses of {@code OutputStream} must provide an
+     * implementation for this method.
+     *
+     * @param b the {@code byte}.
+     * @throws IOException if an I/O error occurs. In particular,
+     *                     an {@code IOException} may be thrown if the
+     *                     output stream has been closed.
+     */
     @Override
-    public void write(byte[] b) { //TODO: 255 limit
+    public void write(int b) throws IOException {
+
+    }
+
+    /**
+     * Writes {@code b.length} bytes from the specified byte array
+     * to this output stream. The general contract for {@code write(b)}
+     * is that it should have exactly the same effect as the call
+     * {@code write(b, 0, b.length)}.
+     *
+     * @param b the data.
+     * @throws IOException if an I/O error occurs.
+     * @see java.io.OutputStream#write(byte[], int, int)
+     */
+    @Override
+    public void write(byte[] b) throws IOException {
         if (b.length == 0) return;
 
-        byte singular = 0;
-        int count = 0;
+        byte data = b[0];
+        int count = 1;
 
         for (int i = 1; i < b.length; i++) {
-            if (singular == b[i] && i != b.length - 1) count++;
+            if (data == b[i] && i != b.length - 1) count++;
             else {
-                write(count);
-
-                if (singular == 0) singular = 1;
-                else singular = 0;
+                write(data, count);
+                data = b[i];
                 count = 1;
             }
         }
--- ATP-Project-PartA/src/IO/MyDecompressorInputStream.java
+++ ATP-Project-PartA/src/IO/MyDecompressorInputStream.java
@@ -2,6 +2,7 @@ package IO;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class MyDecompressorInputStream extends InputStream {
     InputStream in;
@@ -9,10 +10,18 @@ public class MyDecompressorInputStream extends InputStream {
     public MyDecompressorInputStream(InputStream in) {
         this.in = in;
     }
-
-
+    
     @Override
-    public int read() { //TODO
-        return 0;
+    public int read() throws IOException { //TODO
+        int count = -1;
+        int ch = -1;
+        do {
+            ch = in.read();
+            for (int i = 0; i < in.read(); i++) {
+                // add chars
+            }
+        }while(ch != -1);
+        return count;
+
     }
 }
