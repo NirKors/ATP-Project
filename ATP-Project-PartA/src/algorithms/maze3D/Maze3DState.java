3fc64cb3163563f70d314d2a29526fc70f808e48
NirKors <103315566+NirKors@users.noreply.github.com>
2022-06-08 18:20:14 +0300

    Name change of project + Artifacts + Docs

--- ATP-Project-PartA/src/algorithms/maze3D/Maze3DState.java
+++
@@ -1,9 +0,0 @@
-package algorithms.maze3D;
-
-import algorithms.search.AState;
-
-public class Maze3DState extends AState {
-    public Maze3DState(Position3D pos) {
-        this.pos = pos;
-    }
-}
--- ATP-Project-PartA/src/algorithms/search/ASearchingAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package algorithms.search;
-
-public abstract class ASearchingAlgorithm implements ISearchingAlgorithm {
-    protected int nodes_Evaluated = 1; // We always examine at least one state (Start state)
-
-    public int getNumberOfNodesEvaluated() {
-        return nodes_Evaluated;
-    }
-}
