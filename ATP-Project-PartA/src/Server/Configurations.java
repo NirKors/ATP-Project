79848d34f0cdfbce3ecad2a6da819a8608993e60
NirKors <103315566+NirKors@users.noreply.github.com>
2022-06-08 16:59:31 +0300

    Configurations

--- ATP-Project-PartA/src/Server/Configurations.java
+++ ATP-Project-PartA/src/Server/Configurations.java
@@ -6,19 +6,17 @@ import java.util.Properties;
 
 public class Configurations {
 
-    public static Properties props(){
-        try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("config.properties")) {
+    private static Properties prop;
 
-            Properties prop = new Properties();
-
-            //load a properties file from class path, inside static method
-            prop.load(input);
-
-            //get the property value and print it out
-            return prop;
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static Properties getProp() {
+        if (prop == null){
+            try (InputStream input = Configurations.class.getClassLoader().getResourceAsStream("config.properties")) {
+                prop = new Properties();
+                prop.load(input);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return null;
+        return prop;
     }
 }
