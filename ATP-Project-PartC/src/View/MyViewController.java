48b8ecad97808fdbb5eee41cd5d52b9a512d1238
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-06-27 11:31:22 +0300

    Added difficulty menu & button connections in MyView

--- ATP-Project-PartC/src/View/MyViewController.java
+++ ATP-Project-PartC/src/View/MyViewController.java
@@ -5,8 +5,32 @@ import java.awt.event.ActionEvent;
 public class MyViewController implements IView {
 
     //TODO: Link all buttons
-    public void newMaze(javafx.event.ActionEvent actionEvent) {
+    //File menu:
+    public void newButton(javafx.event.ActionEvent actionEvent) {
 
     }
 
+    public void loadButton(javafx.event.ActionEvent actionEvent) {
+
+    }
+
+    public void saveButton(javafx.event.ActionEvent actionEvent) {
+
+    }
+    //Options menu:
+    public void propertiesButton(javafx.event.ActionEvent actionEvent) {
+
+    }
+
+    public void exitButton(javafx.event.ActionEvent actionEvent) {
+
+    }
+
+    public void helpButton(javafx.event.ActionEvent actionEvent) {
+
+    }
+
+    public void aboutButton(javafx.event.ActionEvent actionEvent) {
+
+    }
 }
