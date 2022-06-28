881c63b01cf30ef4448201296019005304bec348
DanielZasla <103315699+DanielZasla@users.noreply.github.com>
2022-06-28 19:19:45 +0300

    Changed MyView.fxml, added About and Help

--- ATP-Project-PartC/src/View/MyViewController.java
+++ ATP-Project-PartC/src/View/MyViewController.java
@@ -4,16 +4,16 @@ import Server.Configurations;
 import ViewModel.MyViewModel;
 import algorithms.mazeGenerators.Maze;
 import algorithms.mazeGenerators.Position;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -31,56 +31,56 @@ public class MyViewController implements IView {
     private static MyViewModel viewModel;
 
     @FXML
+    private BorderPane Displayer;
+    @FXML
     private RadioButton randomMaze, myMazeGen, bfsChoice, dfsChoice, bestChoice;
     @FXML
     private CheckBox soundCheckBox;
     @FXML
     private TextField threadPoolTextField;
+
     public void setViewModel(MyViewModel viewModel) {
         this.viewModel = viewModel;
     }
 
-    public void display(){
-        try{
+    public void display() {
+        try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MyView.fxml"));
             Parent root = fxmlLoader.load();
             Stage stage = new Stage();
             stage.setTitle("Change title");
             stage.setScene(new Scene(root));
             stage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             // TODO: add to logger.
             System.out.println(e.getMessage());
         }
     }
 
 
-
     //File menu:
-    public void newButton(javafx.event.ActionEvent actionEvent){
+    public void newButton(javafx.event.ActionEvent actionEvent) {
         displayDifficultySelection();
     }
 
     private void displayDifficultySelection() {
         Parent root;
         Stage stage;
-        try{
+        try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DifficultyMenu.fxml"));
             root = fxmlLoader.load();
             stage = new Stage();
             stage.setTitle("Select Difficulty");
             stage.setScene(new Scene(root));
             stage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             // TODO: add to logger.
             e.printStackTrace();
             return;
         }
     }
 
-    public void drawMaze(int row, int col){
+    public void drawMaze(int row, int col) {
         viewModel.generateMaze(row, col);
         Maze temp = viewModel.getMaze();
 
@@ -129,80 +129,91 @@ public class MyViewController implements IView {
     public void saveButton(javafx.event.ActionEvent actionEvent) {
         System.out.println("save");
     }
+
     //Options menu:
-    public void propertiesButton(javafx.event.ActionEvent actionEvent){
+    public void propertiesButton(javafx.event.ActionEvent actionEvent) {
         displayPropertiesSelection();
     }
 
     private void displayPropertiesSelection() {
         Parent root;
         Stage stage;
-        try{
+        try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Properties.fxml"));
             root = fxmlLoader.load();
             stage = new Stage();
             stage.setTitle("Properties Menu");
             stage.setScene(new Scene(root));
             stage.show();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             // TODO: add to logger.
             e.printStackTrace();
             return;
         }
     }
 
-
-
-    //TODO: Following buttons don't work.
     public void exitButton(javafx.event.ActionEvent actionEvent) {
-        System.out.println("exit");
+        Platform.exit();
+        //TODO: wtf is a safe exit, and do we need to close the server streams as well?
     }
 
     public void helpButton(javafx.event.ActionEvent actionEvent) {
-        System.out.println("help");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Help");
+        alert.setHeaderText(null);
+        alert.setContentText("Ok we got to hell, now what?\nUse the numpad keys to move around and get to the BFG!");
+        alert.showAndWait();
     }
 
     public void aboutButton(javafx.event.ActionEvent actionEvent) {
-        System.out.println("about");
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.setContentText("MURDERED By Daniel Zaslavsky & Nir Kusronsky\n" +
+                "EXECUTED by the DFS algorithm, that makes the mazes all pretty.\n" +
+                "DESTROYED by the DFS, BFS and BestFirstSearch solving algorithms that help us escape hell!");
+        alert.showAndWait();
     }
 
     //Properties:
-    public void mazeCreatePropertyChoice(javafx.event.ActionEvent event){
-        if(randomMaze.isSelected()){
-            Configurations.getProp().setProperty("mazeGeneratingAlgorithm","SimpleMazeGenerator");
+    public void mazeCreatePropertyChoice(javafx.event.ActionEvent event) {
+        if (randomMaze.isSelected()) {
+            Configurations.getProp().setProperty("mazeGeneratingAlgorithm", "SimpleMazeGenerator");
         }
-        if(myMazeGen.isSelected()){
-            Configurations.getProp().setProperty("mazeGeneratingAlgorithm","MyMazeGenerator");
+        if (myMazeGen.isSelected()) {
+            Configurations.getProp().setProperty("mazeGeneratingAlgorithm", "MyMazeGenerator");
         }
     }
 
-    public void solvePropertyChoice(javafx.event.ActionEvent event){
-        if(bfsChoice.isSelected()){
-            Configurations.getProp().setProperty("mazeSearchingAlgorithm","BreadthFirstSearch");
+    public void solvePropertyChoice(javafx.event.ActionEvent event) {
+        if (bfsChoice.isSelected()) {
+            Configurations.getProp().setProperty("mazeSearchingAlgorithm", "BreadthFirstSearch");
         }
-        if(dfsChoice.isSelected()){
-            Configurations.getProp().setProperty("mazeSearchingAlgorithm","DepthFirstSearch");
+        if (dfsChoice.isSelected()) {
+            Configurations.getProp().setProperty("mazeSearchingAlgorithm", "DepthFirstSearch");
         }
-        if(bestChoice.isSelected()){
-            Configurations.getProp().setProperty("mazeSearchingAlgorithm","BestFirstSearch");
+        if (bestChoice.isSelected()) {
+            Configurations.getProp().setProperty("mazeSearchingAlgorithm", "BestFirstSearch");
         }
+        System.out.println("Current searcher is: " + Configurations.getProp().getProperty("mazeSearchingAlgorithm"));
 
     }
 
     public void threadPoolButton(javafx.event.ActionEvent actionEvent) {
-        if(threadPoolTextField.getText().matches("-?\\d+")) {
+        if (threadPoolTextField.getText().matches("-?\\d+")) {
             Configurations.getProp().setProperty("threadPoolSize", threadPoolTextField.getText());
         }
+        System.out.println("Current generator is: " + Configurations.getProp().getProperty("mazeGeneratingAlgorithm"));
     }
 
 
-    public void soundBox(javafx.event.ActionEvent event){
-        if(soundCheckBox.isSelected()){
+    public void soundBox(javafx.event.ActionEvent event) {
+        if (soundCheckBox.isSelected()) {
             System.out.println("sounds ON");
         } else {
             System.out.println("sounds OFF");
         }
+        System.out.println("Current threadpool size is: " + Configurations.getProp().getProperty("threadPoolSize"));
     }
 
 
